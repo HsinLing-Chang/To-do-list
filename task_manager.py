e70fa8fb800c8ef6da67e0b165ed3b55f7c5b68f
Cindy <cindy870709@gmail.com>
2024-11-05 11:25:51 +0800

    adding list function and other small changes

--- task_manager.py
+++ task_manager.py
@@ -51,12 +51,20 @@ def add_new_tasks(description: str):
 
 def update_task(id, description=None, status: str = None):
     currentData = open_tasks()
+    task_index = id-1
+    status = status.lower()
+    status_allowed = ["todo", "done", "in-progress"]
     try:
         tasks_dict = {
             task["id"]: task for task in currentData["tasks"] if task["id"] == id}
         task = tasks_dict.get(id)  # 用 id 查找
-        update_description = description if description is not None else task["description"]
-        update_status = status if status is not None else task["status"]
+
+        if status in status_allowed:
+            update_description = description if description is not None else task["description"]
+            update_status = status if status is not None else task["status"]
+        else:
+            raise ValueError(f"Status must be one of the following : {
+                             ", ".join(status_allowed)}")
         update_task = {
             "id": id,
             "description": update_description,
@@ -64,19 +72,65 @@ def update_task(id, description=None, status: str = None):
             "createdAt": task["createdAt"],
             "updatedAt": datetime.now().strftime("%d/%m/%Y %H:%M:%S")
         }
-        currentData["tasks"].pop(id-1)
-        currentData["tasks"].insert(id-1, update_task)
+        currentData["tasks"].pop(task_index)
+        currentData["tasks"].insert(task_index, update_task)
         save_tasks(currentData)
+    except ValueError as e:
+        print(e)
     except Exception:
-        print("請輸入正確的id與內容")
+        print("This id cannot be found. Pleas try again.")
+
 
 # delete task with provided id
 
 
 def delete_task(id):
     try:
+        task_index = id-1
         currentData = open_tasks()
-        currentData["tasks"].pop(id - 1)
+        currentData["tasks"].pop(task_index)
         save_tasks(currentData)
     except:
-        print("找不到此id，請重新輸入")
+        print("This id cannot be found, please try again.")
+
+
+def list_all():
+    currentData = open_tasks()
+    if currentData["tasks"]:
+        for task in currentData["tasks"]:
+            print(task)
+    else:
+        print("There are no tasks yet. Please add a new task.")
+
+
+def list_done():
+    currentData = open_tasks()
+    task_lst = [task for task in currentData["tasks"]
+                if task["status"] == "done"]
+    if task_lst:
+        for task in task_lst:
+            print(task)
+    else:
+        print("Tasks cannot be found.")
+
+
+def list_todo():
+    currentData = open_tasks()
+    task_lst = [task for task in currentData["tasks"]
+                if task["status"] == "todo"]
+    if task_lst:
+        for task in task_lst:
+            print(task)
+    else:
+        print("Tasks cannot be found.")
+
+
+def list_in_progress():
+    currentData = open_tasks()
+    task_lst = [task for task in currentData["tasks"]
+                if task["status"] == "in-progress"]
+    if task_lst:
+        for task in task_lst:
+            print(task)
+    else:
+        print("Tasks cannot be found.")
